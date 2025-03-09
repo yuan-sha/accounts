c272c5da8b8dbe431758b4d123e262b1ecb2e903
yuans <yuansha312@gmail.com>
2025-03-09 15:41:20 -0600

    change

--- routes/web/index.js
+++ routes/web/index.js
@@ -12,42 +12,43 @@ router.get('/', (req, res) => {
   res.redirect('/account');
 })
 
-router.get('/account', checkLoginMiddleware, function(req, res, next) {
-  AccountModel.find().sort({time: -1}).exec((err, data) => {
-    if(err){
-      res.status(500).send('Read failed~~~');
-      return;
-    }
-    //响应成功的提示
-    res.render('list', {accounts: data, moment: moment});
-  })
+router.get('/account', checkLoginMiddleware, async function(req, res, next) {
+  try {
+    const data = await AccountModel.find().sort({ time: -1 }).exec();
+    res.render('list', { accounts: data, moment: moment });
+  } catch (err) {
+    res.status(500).send('Read failed~~~');
+  }
 });
+
 router.get('/account/create',checkLoginMiddleware, function(req, res, next) {
   res.render('create');
 });
 
-router.post('/account',checkLoginMiddleware, (req, res) => {
-  AccountModel.create({
-    ...req.body,
-    time: moment(req.body.time).toDate()
-  }, (err, data) => {
-    if(err){
-      res.status(500).send('Failed to insert.~~');
-      return
-    }
-    res.render('success', {msg: 'Added successfully~~~', url: '/account'});
-  })
+router.post('/account', checkLoginMiddleware, async (req, res) => {
+  try {
+    const data = await AccountModel.create({
+      ...req.body,
+      time: moment(req.body.time).toDate(),
+    });
+
+    res.render('success', { msg: 'Added successfully~~~', url: '/account' });
+  } catch (err) {
+    res.status(500).send('Failed to insert.~~');
+  }
 });
 
-router.get('/account/:id', checkLoginMiddleware, (req, res) => {
-  let id = req.params.id;
-  AccountModel.deleteOne({_id: id}, (err, data) => {
-    if(err) {
-      res.status(500).send('Deletion failed~');
-      return;
-    }
-    res.render('success', {msg: 'Deleted successfully~~~', url: '/account'});
-  })
+
+router.get('/account/:id', checkLoginMiddleware, async (req, res) => {
+  try {
+    const id = req.params.id;
+    await AccountModel.deleteOne({ _id: id });
+
+    res.render('success', { msg: 'Deleted successfully~~~', url: '/account' });
+  } catch (err) {
+    res.status(500).send('Deletion failed~');
+  }
 });
+
 
 module.exports = router;
