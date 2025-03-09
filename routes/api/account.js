fcbd2002f0e7d66da6efb6636cd53d613163601d
yuans <yuansha312@gmail.com>
2025-03-09 10:51:55 -0600

    change

--- routes/api/account.js
+++ routes/api/account.js
@@ -1,22 +1,19 @@
-//Import express
 const express = require('express');
-//Import jwt
+
 const jwt = require('jsonwebtoken');
-//Import middleware
+
 let checkTokenMiddleware = require('../../middlewares/checkTokenMiddleware');
 
 const router = express.Router();
-//Import moment
+
 const moment = require('moment');
+
 const AccountModel = require('../../models/AccountModel');
 
-//记账本的列表
 router.get('/account', checkTokenMiddleware, async (req, res) => {
   try {
-    // 读取集合信息，并按时间降序排序
     const data = await AccountModel.find().sort({ time: -1 }).exec();
 
-    // 成功响应
     res.json({
       code: '0000',
       msg: '读取成功',
@@ -34,19 +31,13 @@ router.get('/account', checkTokenMiddleware, async (req, res) => {
 });
 
 
-//新增记录
 router.post('/account', checkTokenMiddleware, async (req, res) => {
   try {
-    // 表单验证（可在此处添加）
-
-    // 插入数据库
     const data = await AccountModel.create({
       ...req.body,
-      // 修改 time 属性的值
       time: moment(req.body.time).toDate()
     });
 
-    // 成功响应
     res.json({
       code: '0000',
       msg: '创建成功',
@@ -64,13 +55,10 @@ router.post('/account', checkTokenMiddleware, async (req, res) => {
 });
 
 
-//删除记录
 router.delete('/account/:id', checkTokenMiddleware, async (req, res) => {
   try {
-    // 获取 id 参数
     const { id } = req.params;
 
-    // 删除数据
     const deleteResult = await AccountModel.deleteOne({ _id: id });
 
     if (deleteResult.deletedCount === 0) {
@@ -81,7 +69,6 @@ router.delete('/account/:id', checkTokenMiddleware, async (req, res) => {
       });
     }
 
-    // 成功响应
     res.json({
       code: '0000',
       msg: '删除成功',
@@ -99,13 +86,10 @@ router.delete('/account/:id', checkTokenMiddleware, async (req, res) => {
 });
 
 
-//获取单个账单信息
 router.get('/account/:id', checkTokenMiddleware, async (req, res) => {
   try {
-    // 获取 id 参数
     const { id } = req.params;
 
-    // 查询数据库
     const data = await AccountModel.findById(id);
 
     if (!data) {
@@ -116,7 +100,6 @@ router.get('/account/:id', checkTokenMiddleware, async (req, res) => {
       });
     }
 
-    // 成功响应
     res.json({
       code: '0000',
       msg: '读取成功',
@@ -134,13 +117,10 @@ router.get('/account/:id', checkTokenMiddleware, async (req, res) => {
 });
 
 
-//更新单个账单信息
 router.patch('/account/:id', checkTokenMiddleware, async (req, res) => {
   try {
-    // 获取 id 参数值
     const { id } = req.params;
 
-    // 更新数据库
     const updateResult = await AccountModel.updateOne({ _id: id }, req.body);
     console.log(updateResult)
     if (updateResult.modifiedCount === 0 && updateResult.matchedCount===0) {
@@ -151,7 +131,6 @@ router.patch('/account/:id', checkTokenMiddleware, async (req, res) => {
       });
     }
 
-    // 再次查询数据库获取单条数据
     const updatedData = await AccountModel.findById(id);
 
     if (!updatedData) {
@@ -162,7 +141,6 @@ router.patch('/account/:id', checkTokenMiddleware, async (req, res) => {
       });
     }
 
-    // 成功响应
     res.json({
       code: '0000',
       msg: '更新成功',
--- routes/web/auth.js
+++ routes/web/auth.js
@@ -1,27 +1,24 @@
 var express = require('express');
+
 var router = express.Router();
-//导入 用户的模型
+
 const UserModel = require('../../models/UserModel');
+
 const md5 = require('md5');
-//注册
+
 router.get('/reg', (req, res) => {
-  //响应 HTML 内容
   res.render('auth/reg');
 });
 
-//注册用户
 router.post('/reg', async (req, res) => {
   try {
-    // 获取请求体数据并加密密码
     const userData = {
       ...req.body,
       password: md5(req.body.password)
     };
 
-    // 创建用户
     await UserModel.create(userData);
 
-    // 注册成功，渲染成功页面
     res.render('success', { msg: '注册成功', url: '/login' });
 
   } catch (error) {
@@ -29,34 +26,26 @@ router.post('/reg', async (req, res) => {
   }
 });
 
-//登录页面
 router.get('/login', (req, res) => {
-  //响应 HTML 内容
   res.render('auth/login');
 });
 
-//登录操作
 router.post('/login', async (req, res) => {
   try {
-    // 获取用户名和密码
     const { username, password } = req.body;
 
-    // 查询数据库
     const data = await UserModel.findOne({
       username: username,
       password: md5(password)
     }).exec();
 
-    // 判断 data 是否存在
     if (!data) {
       res.render('warning', {msg: '账号或密码错误~~', url: '/login'});
     }
 
-    // 写入 session
     req.session.username = data.username;
     req.session._id = data._id;
 
-    // 登录成功响应
     res.render('success', { msg: '登录成功', url: '/account' });
 
   } catch (error) {
@@ -65,7 +54,6 @@ router.post('/login', async (req, res) => {
 });
 
 
-//退出登录
 router.post('/logout', (req, res) => {
   //销毁 session
   req.session.destroy(() => {
